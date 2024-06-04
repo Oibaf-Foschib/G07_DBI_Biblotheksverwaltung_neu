fa825fec240cd3083505004e19519f5b7decf67f
Fabio Bischof <fabio.bischof@student.htl-rankweil.at>
2024-06-04 11:48:27 +0200

    Code für Klasse Book.cs

--- G07_DBI_Biblotheksverwaltung/Book.cs
+++ G07_DBI_Biblotheksverwaltung/Book.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace G07_DBI_Biblotheksverwaltung
 {
-    class Class1
+    public class Book
     {
+        public int BookID { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Genre { get; set; }
+        public int Year { get; set; }
     }
 }
