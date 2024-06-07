20adb171ea4b5b45684fc1505d9a08ea3695271b
Oniichan187 <138072459+Oniichan187@users.noreply.github.com>
2024-06-07 18:12:02 +0200

    übersichtlichere speicherung mit klassen

---
+++ G07_DBI_Biblotheksverwaltung/User-Book-BookLoan.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G07_DBI_Biblotheksverwaltung
+{
+    internal class User_Book_BookLoan
+    {
+        public class Book
+        {
+            public int BookID { get; set; }
+            public string Title { get; set; }
+            public string Author { get; set; }
+            public string Genre { get; set; }
+            public int Year { get; set; }
+        }
+
+        public class User
+        {
+            public int UserID { get; set; }
+            public string Name { get; set; }
+            public string Email { get; set; }
+        }
+
+        public class BookLoan
+        {
+            public string BookTitle { get; set; }
+            public string BookAuthor { get; set; }
+            public string UserName { get; set; }
+            public string UserEmail { get; set; }
+            public DateTime LoanDate { get; set; }
+            public DateTime ReturnDate { get; set; }
+        }
+    }
+}
